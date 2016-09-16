fe137878a9c0708e267050d3f7ee9ad5f64c607d
Jan Noha <nohajc@gmail.com>
2016-09-17 01:12:02 +0200

    Added sync_room node.js server.

---
+++ sync_room/index.js
@@ -0,0 +1,29 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+var crypto = require('crypto')
+var base64url = require('base64url');
+
+function randBytesAsBase64Url(size) {
+	return base64url(crypto.randomBytes(size))
+}
+
+app.get('/', function(req, res) {
+	res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket) {
+	console.log('a user connected');
+	socket.on('disconnect', function() {
+		console.log('user disconnected');
+	});
+
+	socket.on('create_room', function(name) {
+		socket.emit('room_added', name);
+	});
+});
+
+http.listen(4200, function() {
+	console.log('listening on *:4200');
+});
